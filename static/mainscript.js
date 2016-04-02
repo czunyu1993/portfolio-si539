2b0ea49ae6edd380b58c98093ad48ba907096633
Zunyu Chen <czunyu@umich.edu>
2016-04-02 16:11:02 -0400

    modified tablet view

--- static/mainscript.js
+++ static/mainscript.js
@@ -2,14 +2,21 @@
 $(document).ready(function() {
 
 	$( "html" ).removeClass( "loading" );
-	$('nav li a').hover(
-		function() {
-			$(this).parent().css('background-color', '#d9d9d9');
-			$(this).css('color','#000000')
-		},
-		function(){
-			$(this).parent().css('background-color', '#737373');
-			$(this).css('color','white')
-		}
+	$("#large-nav-home > a, #large-nav-project > a, #large-nav-hobby > a, #large-nav-contact > a").hover(
+		
+			function() {
+				$(this).parent().css('background-color', '#d9d9d9');
+				$(this).css('color','#000000')
+			},
+			function(){
+				var fsize = $(this).parent().css('font-size');
+				if (fsize == '14px') {
+					$(this).parent().css('background-color', '#737373');
+					$(this).css('color','white')
+				} else {
+					$(this).parent().css('background-color', '#4d4d4d');
+					$(this).css('color','white')
+				}
+			}
 	);
 });
