4ac843668867a04d109564594c79ed6729b88b79
Zunyu Chen <czunyu@umich.edu>
2016-04-03 15:39:02 -0400

    handled some errors

--- static/mainscript.js
+++ static/mainscript.js
@@ -4,21 +4,31 @@ $(document).ready(function() {
 	$( "html" ).removeClass( "loading" );
 	$("#large-nav-home > a, #large-nav-project > a, #large-nav-hobby > a, #large-nav-contact > a").hover(
 		
-			function() {
-				$(this).parent().css('background-color', '#d9d9d9');
-				$(this).css('color','#000000')
-			},
-			function(){
-				var fsize = $(this).parent().css('font-size');
-				if (fsize == '14px') {
-					$(this).parent().css('background-color', '#737373');
-					$(this).css('color','white')
-				} else {
-					$(this).parent().css('background-color', '#4d4d4d');
-					$(this).css('color','white')
-				}
+		function() {
+			$(this).parent().css('background-color', '#d9d9d9');
+			$(this).css('color','#000000')
+		},
+		function(){
+			var fsize = $(this).parent().css('font-size');
+			if (fsize == '14px') {
+				$(this).parent().css('background-color', '#737373');
+				$(this).css('color','white')
+			} else {
+				$(this).parent().css('background-color', '#4d4d4d');
+				$(this).css('color','white')
 			}
+		}
 	);
+
+	$(".hobbyimg").mouseenter(function() {
+		$(this).css('opacity', '0.3');
+		$(this).siblings(".imginfo").show();
+	});
+	$(".hobbyimg").mouseleave(function() {
+		$(this).css('opacity', '1');
+		$(this).siblings(".imginfo").hide();
+	});
+
 });
 
 
@@ -27,19 +37,4 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
     document.getElementById("dropdownbtn").classList.toggle("movedropbtn");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
 }
